cbfdab60afeb82a3a06be599b4887ebed6451679
Huynh Quoc Khanh aka Kan <70838508+Kan-Kzeit@users.noreply.github.com>
2023-03-30 12:44:03 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,29 +1,24 @@
-require("onScreenKeyboard")         -- include the onScreenKeyboard.lua file
---create a textfield for the content created with the keyoard
+require("onScreenKeyboard")
+
 local textField = display.newText("", 0, 0, display.contentWidth, 100, native.systemFont, 50)
 textField:setTextColor(255, 255, 255)
---create an instance of the onScreenKeyboard class without any parameters. This creates a keyboard
---with the default values. You can manipulate the visual representation of the keyboard by passing a table to the new() function.
---Read more about this in the section "customizing the keyboard style"
+
 local keyboard = onScreenKeyboard:new()
 --create a listener function that receives the events of the keyboard
 local listener = function(event)
     if (event.phase == "ended") then
-        textField.text = keyboard:getText()       --update the textfield with the current text of the keyboard
-        -- textField:setReferencePoint(display.TopLeftReferencePoint)
+        textField.text = keyboard:getText()
         textField.anchorX = 0
         textField.anchorY = 0
         textField.x = 0
         textField.y = 0
-        --check whether the user finished writing with the keyboard. The inputCompleted
-        --flag of  the keyboard is set to true when the user touched its "OK" button
+
         if (event.target.inputCompleted == true) then
             print("Input of data complete...")
             keyboard:destroy()
         end
     end
 end
---let the onScreenKeyboard know about the listener
+
 keyboard:setListener(listener)
---show a keyboard with small printed letters as default. Read more about the possible values for keyboard types in the section "possible keyboard types"
 keyboard:drawKeyBoard(keyboard.keyBoardMode.letters_small)
